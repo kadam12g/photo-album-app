14043e5f46537970cf92e6f1fbe3c6f75bc3d728
Kiss Adam <kissadam@sch.bme.hu>
2025-04-07 22:40:36 +0200

    curl bme.hu

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def hello_world():
     return 'Hello, World! This photo album app is running on Kubernetes!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=10099)
 
