bd3b3dea600d76265af3342ad80ffbd51680c484
Kiss Adam <kissadam@sch.bme.hu>
2025-04-07 03:51:59 +0200

    Add GitHub workflow and simplified test app

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This photo album app is running on Kubernetes!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
